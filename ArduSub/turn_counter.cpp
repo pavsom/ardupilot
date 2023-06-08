3e188a864b897fcb916d8865491e8321c2e4a739
Vexy-AA <vexy1337@gmail.com>
2023-06-08 14:29:51 +0300

    sub.turnCounter. added extNav messages speed counted from thrust

--- ArduSub/turn_counter.cpp
+++ ArduSub/turn_counter.cpp
@@ -1,11 +1,74 @@
 // Code by Rustom Jehangir: rusty@bluerobotics.com
 
 #include "Sub.h"
-
+#include <stdio.h>
 // Count total vehicle turns to avoid tangling tether
+
+float bodyInertia = 0.6f;
+float forwardThrust = 0.0f;
+float rightThrust = 0.0f;
+float upThrust = 0.0f;
+
+float forwardVelRatio = 1.0f;
+float rightVelRatio = 1.0f;
+float upVelRatio = 1.0f;
+uint32_t lastEstimate = 0;
+uint32_t dT = 0;
+float forwardVelocity = 0.0f;
+float rightVelocity = 0.0f;
+float upVelocity =  0.0f;
+float dF = 0.0f;
+float dR = 0.0f;
+float trim = 0;
+
 void Sub::update_turn_counter()
 {
     // Determine state
+    //AP::ahrs().writeExtNavVelData(vel_corrected, _frontend.get_vel_noise(), time_ms, _frontend.get_delay_ms());
+    uint32_t tnow = AP_HAL::millis();
+    if (!trim){
+        //trim = abs(1485 - (motors.get_pwm_output_max() + motors.get_pwm_output_min())/2);
+        //trim = trim / ((motors.get_pwm_output_max() - motors.get_pwm_output_min())/2);
+        
+    }
+    forwardThrust = forwardThrust * bodyInertia + (motors.get_forward() - trim) * (1 - bodyInertia);
+    rightThrust = rightThrust * bodyInertia + (motors.get_lateral() - trim)  * (1 - bodyInertia);
+    upThrust = upThrust * bodyInertia + (motors.get_throttle_in_bidirectional() - trim) * (1 - bodyInertia);
+    //if (forwardThrust < -0.1) forwardThrust = 0;
+    //if (rightThrust < -0.1) rightThrust = 0;
+    //if (upThrust < -0.1) upThrust = 0;
+    dT = (tnow - lastEstimate);
+    lastEstimate = tnow;
+    //printf("turn counter dT = %ld\n\r",dT);
+    forwardVelocity = forwardThrust * forwardVelRatio;
+    rightVelocity = rightThrust * rightVelRatio;
+    upVelocity = upThrust * upVelRatio;
+    dF = forwardVelocity * dT;
+
+    /* printf("\n\rbodyFrame X=%+f; Y=%+f; Z=%+f--------\n\r",
+    forwardThrust,rightThrust,upThrust); */
+
+    Vector3f vel_corrected = ahrs.get_rotation_body_to_ned() * Vector3f(forwardVelocity, rightVelocity, --upVelocity); 
+        /* printf("nedFrame1 X=%+f; Y=%+f; Z=%+f\n\r",
+        vel_corrected.x,vel_corrected.y,vel_corrected.z); */
+
+/*     Vector3f vel_corrected2 = ahrs.get_rotation_body_to_ned().transposed() * Vector3f(forwardVelocity, rightVelocity, upVelocity);
+        printf("nedFrameT X=%+f; Y=%+f; Z=%+f\n\r",
+        vel_corrected2.x,vel_corrected2.y,vel_corrected2.z);
+
+    Vector3f vel_corrected3 = ahrs.get_rotation_body_to_ned() * Vector3f(forwardVelocity, rightVelocity, -upVelocity);
+        printf("nedFrame3 X=%+f; Y=%+f; Z=%+f\n\r",
+        vel_corrected3.x,vel_corrected3.y,vel_corrected3.z);
+    
+    Vector3f vel_corrected4 = ahrs.get_rotation_body_to_ned().transposed() * Vector3f(forwardVelocity, rightVelocity, -upVelocity);
+        printf("nedFrame4 X=%+f; Y=%+f; Z=%+f\n\r",
+        vel_corrected4.x,vel_corrected4.y,vel_corrected4.z); */
+
+    vel_corrected.z = 0;
+    AP::ahrs().writeExtNavVelData(vel_corrected, 0.3, tnow, 10);
+
+    
+
     // 0: 0-90 deg, 1: 90-180 deg, 2: -180--90 deg, 3: -90--0 deg
     uint8_t turn_state;
     if (ahrs.yaw >= 0.0f && ahrs.yaw < radians(90)) {
