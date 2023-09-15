2d6f45213f38a43c36d540f3d9265b95560f791f
Pavel Somov <pavsome@mail.ru>
2023-09-15 09:34:33 +0300

    Tiny fix for previous commit

--- libraries/AP_Notify/DroneCAN_RGB_LED.cpp
+++ libraries/AP_Notify/DroneCAN_RGB_LED.cpp
@@ -76,7 +76,7 @@ bool DroneCAN_RGB_LED::hw_set_rgb_id(rgbHz* pLeds, uint8_t send_len, uint8_t str
     uavcan_equipment_indication_LightsCommand msg {};
     msg.commands.len = send_len;
     uint8_t inc = 0;
-    for(uint8_t i = 0; i < send_len; i++){
+    for(uint8_t i = 0; i < struct_len; i++){
         if (pLeds[i].flag){
             msg.commands.data[inc].light_id = pLeds[i].id;
             msg.commands.data[inc].color.red = pLeds[i].r >> 3;
